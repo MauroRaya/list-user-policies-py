5e6d04cb0aa9f2884d226a5e09db31a43757af1f
MauroRaya <maurorayabr@gmail.com>
2026-01-15 09:23:30 -0300

    feat: add list attached user policy names function

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import boto3
 def list_user_policy_names(client: boto3.client, username: str) -> list[str]:
     response = client.list_user_policies(UserName=username)
     return response['PolicyNames']
+
+
+def list_attached_user_policy_names(client: boto3.client, username: str) -> list[str]:
+    response = client.list_attached_user_policies(UserName=username)
+    policies = response['AttachedPolicies']
+    return [policy['PolicyName'] for policy in policies]
