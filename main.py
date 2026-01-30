03fd41b69bb9d1608b1c350d7b62e969480e5188
MauroRaya <maurorayabr@gmail.com>
2026-01-30 19:40:02 -0300

    refactor: make list_attached_user_policy_names function async

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ async def list_user_policy_names(client: Session.client, username: str) -> list[
     return response['PolicyNames']
 
 
-def list_attached_user_policy_names(client: boto3.client, username: str) -> list[str]:
-    response = client.list_attached_user_policies(UserName=username)
+async def list_attached_user_policy_names(client: Session.client, username: str) -> list[str]:
+    response = await client.list_attached_user_policies(UserName=username)
     policies = response['AttachedPolicies']
     return [policy['PolicyName'] for policy in policies]
 
