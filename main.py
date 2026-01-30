86682842b966916f8d0306d9646081bed06420cb
MauroRaya <maurorayabr@gmail.com>
2026-01-30 19:39:12 -0300

    refactor: make list_user_policy_names function async

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import csv
 from aioboto3.session import Session
 
 
-def list_user_policy_names(client: boto3.client, username: str) -> list[str]:
-    response = client.list_user_policies(UserName=username)
+async def list_user_policy_names(client: Session.client, username: str) -> list[str]:
+    response = await client.list_user_policies(UserName=username)
     return response['PolicyNames']
 
 
